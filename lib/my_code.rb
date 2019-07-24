4adb05993febe7302a7b1bb5a18a38c445023c5b
Alec Hall <alecnhall@gmail.com>
2019-07-24 19:44:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,6 +34,7 @@ def map_to_square(source_array)
   end
   return new
 end
+
 def reduce_to_total(source_array, starting_point = 0)
   i = 0
   sum = starting_point
@@ -43,6 +44,7 @@ def reduce_to_total(source_array, starting_point = 0)
   end
   return sum
 end
+
 def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.length do
