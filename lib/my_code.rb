acb26f02f57ab84dece08671c9ea1ea480708840
Alec Hall <alecnhall@gmail.com>
2019-07-24 18:10:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,7 @@ def map_to_square(source_array)
   source_array.map{ |i| i ** 2}
 end
 def reduce_to_total(source_array)
-  source_array.reduce(0) { |sum, n| sum + n }
+  source_array.reduce { |sum, n| sum + n }
 end
 def reduce_to_all_true(source_array)
   source_array.reduce { }
